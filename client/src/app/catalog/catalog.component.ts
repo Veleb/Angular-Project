ceaa1c220aa27eb861456e63a225e2f66a14e11a
Veleb <philipradev9@gmail.com>
2024-11-24 00:05:45 +0200

    Added product card and basic category filtering logic, and some styles

--- client/src/app/catalog/catalog.component.ts
+++ client/src/app/catalog/catalog.component.ts
@@ -1,30 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../types';
+import { ProductCardComponent } from '../helper/product-card/product-card.component';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [],
+  imports: [ ProductCardComponent ],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
 export class CatalogComponent implements OnInit {
   products: Product[] = [];
+  selectedCategories: String[] = [];
+  filteredProducts: Product[] = [];
 
   constructor(private productService: ProductService) {}
-  
-  selectedCategories: String[] = [];
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe({
       next: (data) => {
         this.products = data;
+        this.filteredProducts = [...data]; 
       },
       error: (error) => {
-        console.error(`Error fetching products: ${error}`)
+        console.error(`Error fetching products: ${error}`);
       }
-    })
+    });
   }
 
   categories = [
@@ -40,16 +42,23 @@ export class CatalogComponent implements OnInit {
     'Music & Movies'
   ];
 
-  // add logic if selectedCategories iz empty to be all categories
-
   onCategoryChange(event: Event): void {
-    const target = event.target as HTMLInputElement; 
-    
+    const target = event.target as HTMLInputElement;
+
     if (target.checked) {
       this.selectedCategories.push(target.id);
+    } else {
+      this.selectedCategories = this.selectedCategories.filter(
+        (category) => category !== target.id
+      );
     }
-    else {
-      this.selectedCategories = this.selectedCategories.filter(category => category !== target.id);
+
+    if (this.selectedCategories.length !== 0) {
+      this.filteredProducts = this.products.filter((product) =>
+        this.selectedCategories.includes(product.category)
+      );
+    } else {
+      this.filteredProducts = [...this.products];
     }
   }
 }
