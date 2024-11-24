d2e37ae9e079fe6f4f4b5f12b798658abe022cdc
Veleb <philipradev9@gmail.com>
2024-11-24 02:18:13 +0200

    Added error page for non-existing routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
@@ -10,5 +11,5 @@ export const routes: Routes = [
   { path: "catalog", component: CatalogComponent },
   { path: "register", component: RegisterComponent },
   { path: "login", component: LoginComponent },
-  // { path: "**", ErrorComponent }
+  { path: "**", component: ErrorComponent }
 ];
