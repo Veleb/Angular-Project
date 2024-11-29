eb958fe6195fc423ce5f4d85e60b91a6990bc2fd
Veleb <philipradev9@gmail.com>
2024-11-29 23:36:26 +0200

    Fixed the auth guard and added it for some routes.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,24 +2,24 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { RegisterComponent } from './user/register/register.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './user/login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { DetailsComponent } from './products/details/details.component';
-import { authGuard } from './guards/auth.guard';
 import { EditComponent } from './products/edit/edit.component';
 import { CreateComponent } from './products/create/create.component';
 import { ProfileComponent } from './user/profile/profile.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "catalog", component: CatalogComponent },
   { path: "product", children: [
-    { path: 'create', component: CreateComponent },
+    { path: 'create', component: CreateComponent, canActivate: [authGuard] },
     { path: ":id", component: DetailsComponent },
-    { path: "edit/:id", component: EditComponent }
+    { path: "edit/:id", component: EditComponent, canActivate: [authGuard] }
   ] },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
   { path: "register", component: RegisterComponent },
   { path: "login", component: LoginComponent },
   { path: "**", component: ErrorComponent }
--- client/src/app/guards/auth.guard.ts
+++ client/src/app/guards/auth.guard.ts
@@ -1,11 +1,13 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from '../user/user.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
 
   const router: Router = inject(Router)
+  const userService: UserService = inject(UserService);
 
-  const isLogged: boolean = false; // change to auth service check if the user is logged in 
+  const isLogged: boolean = userService.isLogged; 
 
   if (isLogged) {
     return true;
